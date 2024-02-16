2bdde7579ea86f7d0d7e6fc79e5521a86b049e81
Rahul SK <63040146+Rahulraymond@users.noreply.github.com>
2024-02-16 11:49:37 +0530

    slides for Maysa done.

---
+++ js.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Change slide every 5 seconds
